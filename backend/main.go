d55d0b90f3e156f899201f4ac5bf335a9ed8f1e4
Taisei2 <tnbox3588git@gmail.com>
2026-06-02 20:33:42 +0900

    クエリ文字列パラメータ

--- backend/main.go
+++ backend/main.go
@@ -9,22 +9,14 @@ import (
 func main() {
 	router := gin.Default()
 
-	// :name のようにコロンで始まる部分は「動的パラメータ」と呼んで、URLのその位置に来た値を変数として取り出せる。
-	router.GET("/user/:name", func(c *gin.Context) {
-		// c.Param() で、:name と書いた部分の実際の値を取得する。
-		name := c.Param("name")
-		// c.String() は文字列としてレスポンスを返すメソッドです。
-		// 第二引数に、フォーマットが使える。
-		c.String(http.StatusOK, "Hello %s", name)
-	})
+	router.GET("/welcome", func(c *gin.Context) {
 
-	// *action は / を含めて残り全部にマッチします。
-	// （例： GET /user/Natti/send -> /send が取得される） -
-	router.GET("/user/:name/*action", func(c *gin.Context) {
-		name := c.Param("name")
-		action := c.Param("action")
-		message := name + " is " + action
-		c.String(http.StatusOK, message)
+		// c.DefaultQuery()はクリエ―パラメータの値を返します。 キーが存在しない場合は、第二引数の値を返す
+		firstname := c.DefaultQuery("firstname", "Guest")
+		// c.Query()は、クエリパラメータの値を返します。 キーが存在しない場合は、空文字を返す
+		lastname := c.Query("lastname")
+
+		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
 	})
 
 	router.Run(":8080")
