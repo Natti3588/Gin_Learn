00ae02a26c3afb16c4aac58582ba88ddb915d464
Taisei2 <tnbox3588git@gmail.com>
2026-06-02 20:26:03 +0900

    パス内パラメータ

--- backend/main.go
+++ backend/main.go
@@ -1,58 +1,31 @@
 package main
 
-// net/http 標準ライブラリで、HTTPステータスコードを使うために読み込む。
 import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// ハンドラ関数。 Contextがリクエストとレスポンスの情報を持つオブジェクト。
-
-func getting(c *gin.Context) {
-	// c.JSON() はレスポンスをJSON形式で返す意味で、第一引数にHTTPステータスコード、
-	// 第二引数が中身。
-	// gin.H は map[string]interface{}のショートカットで、JSONのキーと値を簡単に書ける。
-	c.JSON(http.StatusOK, gin.H{"method": "GET"})
-}
-
-func posting(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"method": "POST"})
-}
-
-func putting(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"method": "PUT"})
-}
-
-func deleting(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"method": "DELETE"})
-}
-
-func patching(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"method": "PATCH"})
-}
-
-func head(c *gin.Context) {
-	// c.Status() は body を返さずに、ステータスコードだけを設定します。
-	c.Status(http.StatusOK)
-}
-
-func options(c *gin.Context) {
-	c.Status(http.StatusOK)
-}
-
 func main() {
-	// ルータを作成しています。 Default では Logger と Recovery 2つのミドルウェアが組み込まれたルーターを返す
 	router := gin.Default()
 
-	// ルーティングを定義しています。 （例： GET /someGetが呼ばれたら、ハンドラ関数のgetting()が呼ばれて、JSON形式の{"method" : "GET"}が返される）
-	router.GET("/someGet", getting)
-	router.POST("/somePost", posting)
-	router.PUT("/somePut", putting)
-	router.DELETE("/someDelete", deleting)
-	router.PATCH("/somePatch", patching)
-	router.HEAD("/someeEad", head)
-	router.OPTIONS("/someOptions", options)
+	// :name のようにコロンで始まる部分は「動的パラメータ」と呼んで、URLのその位置に来た値を変数として取り出せる。
+	router.GET("/user/:name", func(c *gin.Context) {
+		// c.Param() で、:name と書いた部分の実際の値を取得する。
+		name := c.Param("name")
+		// c.String() は文字列としてレスポンスを返すメソッドです。
+		// 第二引数に、フォーマットが使える。
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+
+	// *action は / を含めて残り全部にマッチします。
+	// （例： GET /user/Natti/send -> /send が取得される） -
+	router.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
 
 	router.Run(":8080")
 }
