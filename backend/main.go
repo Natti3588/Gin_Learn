899a7b97386c171d130af0ea15255e13e83a1dfc
Taisei2 <tnbox3588git@gmail.com>
2026-06-04 10:27:50 +0900

    単一ファイル

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,15 +12,30 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.POST("/post", func(c *gin.Context) {
-		ids := c.QueryMap("ids") // URLクエリ文字列をマップとして受け取る
-		names := c.PostForm("names")
+	// 8 << 20 は8MBです。 multipart処理時にメモリに保持する上限です。
+	router.MaxMultipartMemory = 8 << 20
 
-		fmt.Printf("ids: %v; names: %v\n", ids, names)
-		c.JSON(http.StatusOK, gin.H{
-			"ids":   ids,
-			"names": names,
-		})
+	router.POST("/upload", func(c *gin.Context) {
+
+		// フォームの file フィールドからアップロードファイルを取得します。
+		// 失敗した場合は 400 bad Request を JSONで返して終了
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// 受け取ったファイル名をサーバーのログに出力
+		log.Println(file.Filename)
+
+		// 保存先のパスを組み立てる。
+		dst := filepath.Join("./files/", filepath.Base(file.Filename))
+
+		// ファイルを ./files/ ディレクトリに保存
+		c.SaveUploadedFile(file, dst)
+
+		// 成功メッセージを返す
+		c.String(http.StatusOK, fmt.Sprintf("`%s` uploaded!", file.Filename))
 	})
 
 	router.Run(":8080")
