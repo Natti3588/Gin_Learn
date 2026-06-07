584f84f674bca7a99fa9691472534832b6e47b0b
Taisei2 <tnbox3588git@gmail.com>
2026-06-07 17:19:39 +0900

    バインディング

---
+++ backend/cmd/Binding/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type LoginForm struct {
+	User     string `form:"user" binding:"required"`
+	Password string `form:"password" binding:"required"`
+}
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/login", func(c *gin.Context) {
+		var form LoginForm
+
+		if err := c.ShouldBind(&form); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "logged in"})
+	})
+
+	router.Run(":8080")
+}
