17de0afc5622c6b5d9df3bc5d1e154f6c4889e54
Taisei2 <tnbox3588git@gmail.com>
2026-06-09 11:44:47 +0900

    feat: Reactプロジェクトを立ち上げ。 同時に Biome と TailWind CSS を依存として追加

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="bg-blue-600">こんにちは！</h1>
+    </>
+  )
+}
+
+export default App
