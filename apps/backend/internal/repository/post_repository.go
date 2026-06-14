1338b20dde5e56c330450acc844a0df87c9231ee
Taisei2 <tnbox3588git@gmail.com>
2026-06-14 20:55:34 +0900

    feat: データ操作を行う repository ファイルを追加

--- apps/backend/internal/repository/post_repository.go
+++ apps/backend/internal/repository/post_repository.go
@@ -1,8 +1,25 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"backend/internal/model"
+
+	"gorm.io/gorm"
+)
 
 type PostRepository struct {
 	db *gorm.DB
 }
 
+func NewPostRepository(db *gorm.DB) *PostRepository {
+	return &PostRepository{db: db}
+}
+
+func (r *PostRepository) FindAll() ([]model.Post, error) {
+	var posts []model.Post
+	err := r.db.Find(&posts).Error
+	return posts, err
+}
+
+func (r *PostRepository) Create(post *model.Post) error {
+	return r.db.Create(post).Error
+}
